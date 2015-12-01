edf337dde7d202daa6e429536999edda957b1d24
Yao-Yuan Mao <yymao@stanford.edu>
2015-11-30 18:41:51 -0800

    escape double quote; display more authors

--- arxivbot.py
+++ arxivbot.py
@@ -5,9 +5,12 @@ from common import escape
 _error_msg = '''*Syntax*: /arxiv [arxiv id] [[comments]]
 Please provide a valid arxiv ID.'''
 
-_output_template = u'{3}> [<http://arxiv.org/abs/{0}|{0}>] *{1}* by {2} et al.'
+_output_template = u'{3}> [<http://arxiv.org/abs/{0}|{0}>] *{1}* by {2}'
 
 _payload_template = '{{"channel": "{0}", "username": "{1} via arxivbot", "text": "{2}", "link_names": 1}}'
+
+def _format_authors(authors, n=4):
+    return ', '.join(authors[:n]) + (' et al.' if len(authors) > n else '')
 
 def program(data):
     query, __, comment = data['text'].strip().partition(' ')
@@ -25,7 +28,8 @@ def program(data):
 
     output = _output_template.format(entry['key'], \
             escape(re.sub(r'\s+', u' ', entry['title'], re.U)), \
-            escape(entry['first_author']), escape(comment)).encode('utf-8')
+            escape(_format_authors(entry['authors'])), \
+            escape(comment)).encode('utf-8')
 
     payload = _payload_template.format(data['channel_id'], data['user_name'], \
             output)
--- common.py
+++ common.py
@@ -4,7 +4,7 @@ from urllib import urlopen
 _error_template = '*Something went wrong and this result couldn\'t be posted directly onto the channel.*\n{0}\nDebugging info: {1}\npayload = {2}'
 
 def escape(s):
-    return s.replace('&', '&amp;').replace('<', '&lt;').replace('>', '&gt;')
+    return s.replace('&', '&amp;').replace('<', '&lt;').replace('>', '&gt;').replace('"', r'\"')
 
 def run(program, data, token=None, incoming_url=None):
     if token and data['token'] != token:
